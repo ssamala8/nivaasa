0245221079c72562f8439bd8419d81f693c3b0a9
ssamala8 <sravankumarsamala8@gmail.com>
2025-10-19 12:55:23 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('file-loader?name=[name].[ext]!./index.html'); // Webpack: to include index.html
-import React from 'react'; // ✅ React 18+
-import ReactDOM from 'react-dom/client'; // ✅ React 18+
-import App from './App';
-
-// ✅ Make sure the element with id="root" exists in index.html
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
